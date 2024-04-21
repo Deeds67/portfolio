7a9c1e0c0a83788d294d0ab75298ed5bb266a3c8
Pierre <pierremarais67@gmail.com>
2024-04-21 14:46:35 +0200

    Added profile picture

--- src/components/ui/AboutMe/index.tsx
+++ src/components/ui/AboutMe/index.tsx
@@ -1,5 +1,6 @@
 import Button from "../Button"
 import Tags from "../Tags"
+import Image from "next/image"
 
 interface Props {
     tags: string[]
@@ -9,22 +10,23 @@ const AboutMe = (props: Props) => {
 
     return (
         <div className="flex flex-col max-w-xs bg-white rounded-xl p-2 gap-4">
-        <p className="font-semibold pl-5 text-lg">Pierre Marais</p>
-        <p className="pl-5">Senior Software Engineer - DE</p>
-        <div className="text-sm bg-gray-50 rounded p-5">
-            Software engineer with 9+ years of experience in building web apps. Skilled in Kotlin, React, Python, Typescript, PostgreSQL, Kafka and Kubernetes.
-            <div className="mt-4">
-                <Tags tags={props.tags}></Tags>
+            <div className="flex justify-center mt-3">
+                <Image src="/linkedinprofilepic.jpeg" width={150} height={150} className="rounded-full border-4 border-gray-400"></Image>
             </div>
+            <p className="font-semibold pl-5 text-lg">Pierre Marais</p>
+            <p className="pl-5">Senior Software Engineer - DE</p>
+            <div className="text-sm bg-gray-50 rounded p-5">
+                Software engineer with 9+ years of experience in building web apps. Skilled in Kotlin, React, Python, Typescript, PostgreSQL, Kafka and Kubernetes.
+                <div className="mt-4">
+                    <Tags tags={props.tags}></Tags>
+                </div>
+            </div>
+            <Button className="flex justify-center items-center gap-3 w-auto text-white bg-blue-600 hover:bg-blue-500 ring-offset-2 ring-blue-600 focus:ring shadow rounded-lg">
+                Download CV
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-4">
+                                <path fill="currentColor" d="M13 10h5l-6 6l-6-6h5V3h2zm-9 9h16v-7h2v8a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-8h2z"></path></svg>
+            </Button>
         </div>
-        <Button className="flex justify-center items-center gap-3 w-auto text-white bg-blue-600 hover:bg-blue-500 ring-offset-2 ring-blue-600 focus:ring shadow rounded-lg">
-            Download CV
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-4">
-                              <path fill="currentColor" d="M13 10h5l-6 6l-6-6h5V3h2zm-9 9h16v-7h2v8a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-8h2z"></path></svg>
-        </Button>
-
-        </div>
-
     )
 }
 
