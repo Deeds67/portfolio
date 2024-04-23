460742ad5e7edfd769c096a2d8084a7495fe5fda
Pierre <pierremarais67@gmail.com>
2024-04-23 14:02:19 +0200

    Empty Projects layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
 import Experience from "@/components/ui/Experience";
 import Education from "@/components/ui/Education";
-import PageLayout from "@/components/layout/PageLayout";
+import AboutMe from "@/components/ui/AboutMe";
+import Introduction from "@/components/ui/Introduction";
+import NavBar from "@/components/ui/NavBar";
 
 export default function Home() {
   return (
-    <PageLayout>
-      <Experience></Experience>
-      <Education></Education>
-    </PageLayout>
+    <main className="min-h-screen xs:p-24">
+      <NavBar></NavBar>
+      <div className="flex flex-col lg:flex-row justify-center gap-3 pb-3">
+        <div className="flex flex-col gap-3">
+            <Introduction></Introduction>
+            <AboutMe></AboutMe>
+        </div>
+        <div className="flex flex-col gap-3">
+        < Experience></Experience>
+<           Education></Education>
+        </div>
+      </div>
+    </main>
   );
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,19 @@
-import PageLayout from "@/components/layout/PageLayout"
+import AboutMe from "@/components/ui/AboutMe";
+import Introduction from "@/components/ui/Introduction";
+import NavBar from "@/components/ui/NavBar";
 
 const Projects = () => {
     return (
-        <PageLayout></PageLayout>
-    )
+        <main className="min-h-screen xs:p-24">
+            <NavBar></NavBar>
+            <div className="flex flex-col lg:flex-row justify-center gap-3 pb-3">
+                <div className="hidden lg:flex flex-col gap-3">
+                    <Introduction></Introduction>
+                    <AboutMe></AboutMe>
+                </div>
+            </div>
+        </main>
+    );
 }
 
 export default Projects
